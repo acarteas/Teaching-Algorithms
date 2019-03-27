415b97f7850fdaad4967ec1b80e83682fe54d5ed
Adam Carter <mail@adam-carter.com>
2019-03-27 16:50:43 -0700

    finished lumber cutting example

--- lectures/03 - dynamic programming/01 - fibonacci and rod cutting/lumber.py
+++ lectures/03 - dynamic programming/01 - fibonacci and rod cutting/lumber.py
@@ -42,14 +42,32 @@ def calculate_price_bu(length):
     prices = [2, 3, 4, 5, 7, 9, 10, 12]
     cuts = [6, 8, 10, 12, 16, 18, 20, 24]
     best_prices = []
+
+    #board lengths less than 6
     for i in range(6):
         best_prices.append(0)
+
+    #base boards that make money
     for i in range(len(cuts) - 1):
         for j in range(cuts[i + 1] - cuts[i]):
             best_prices.append(prices[i])
     best_prices.append(12)
+
+    for i in range(25, length + 1):
+        potential_prices = []
+        for cut_index in range(len(cuts)):
+            cut_length = cuts[cut_index]
+            potential_prices.append(
+                prices[cut_index] + #the money made from making current cut
+                best_prices[i - cut_length] #max money from leftover piece
+                )
+        best_cut_price = max(potential_prices)
+        best_prices.append(best_cut_price)
+    return best_prices[length]
+
+
     return best_prices
 
-print(calculate_price(1000, {}))
-result = calculate_price_bu(50)
+print(calculate_price(55, {}))
+print(calculate_price_bu(55))
 print("done")
