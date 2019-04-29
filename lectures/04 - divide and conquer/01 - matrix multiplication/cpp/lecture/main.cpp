9be1a8a8494d60ff6db0627f9ea09e566987a656
Adam Carter <mail@adam-carter.com>
2019-04-29 07:20:47 -0700

    added blank starter code for today

---
+++ lectures/04 - divide and conquer/01 - matrix multiplication/cpp/lecture/main.cpp
@@ -0,0 +1,13 @@
+#include <queue>
+#include <unordered_map>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main(void)
+{
+   return 0;
+}
